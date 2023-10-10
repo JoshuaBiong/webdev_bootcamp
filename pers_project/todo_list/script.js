162cfb65e3a22b1131439f918f1f760b72e42ecd
Joshua Biong <joshuabiong7@gmail.com>
2023-10-11 02:56:59 +0800

    adding checked and removed funtion

--- pers_project/todo_list/script.js
+++ pers_project/todo_list/script.js
@@ -1,8 +1,10 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
-function addTask() {
 
+// adding task function
+
+function addTask() {
     let arr = [];
     if (inputBox.value === '') {
         alert("Write Something")
@@ -10,27 +12,19 @@ function addTask() {
         let li = document.createElement('li')
         li.innerHTML = inputBox.value
         listContainer.appendChild(li)
-
         arr.push(inputBox.value)
-
-
-
-
-
-
         console.log(arr)
-
-
-
-
     }
-
-    // arr.push(inputBox.value)
-
-    // let tempAr = []
-    // tempAr.push(arr++)
-
-
     inputBox.value = '';
 
 }
+// Checked and remove list function
+
+
+listContainer.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle("checked")
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove()
+    }
+}, false)
