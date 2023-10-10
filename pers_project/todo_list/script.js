7bf480e9a330f7fa653d710b1006ec07505cba60
Joshua Biong <joshuabiong7@gmail.com>
2023-10-11 03:29:27 +0800

    storing data from local storage

--- pers_project/todo_list/script.js
+++ pers_project/todo_list/script.js
@@ -12,10 +12,16 @@ function addTask() {
         let li = document.createElement('li')
         li.innerHTML = inputBox.value
         listContainer.appendChild(li)
-        arr.push(inputBox.value)
-        console.log(arr)
+        let span = document.createElement('span')
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+
+        // arr.push(inputBox.value)
+        // console.log(arr)
     }
     inputBox.value = '';
+    saveData();
+
 
 }
 // Checked and remove list function
@@ -24,7 +30,22 @@ function addTask() {
 listContainer.addEventListener('click', function(e) {
     if (e.target.tagName === 'LI') {
         e.target.classList.toggle("checked")
+        saveData();
     } else if (e.target.tagName === 'SPAN') {
         e.target.parentElement.remove()
+        saveData();
     }
 }, false)
+
+// saving data from local storage
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+//showing data
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+showTask();
