12d9bba8b35e4806d0058234e62ea1e8f756a1b8
Joshua Biong <joshuabiong7@gmail.com>
2023-10-10 20:58:47 +0800

    trying to push the value of inpute box in a array

--- pers_project/todo_list/script.js
+++ pers_project/todo_list/script.js
@@ -2,13 +2,35 @@ const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
 function addTask() {
+
+    let arr = [];
     if (inputBox.value === '') {
         alert("Write Something")
     } else {
         let li = document.createElement('li')
         li.innerHTML = inputBox.value
         listContainer.appendChild(li)
+
+        arr.push(inputBox.value)
+
+
+
+
+
+
+        console.log(arr)
+
+
+
+
     }
 
+    // arr.push(inputBox.value)
+
+    // let tempAr = []
+    // tempAr.push(arr++)
+
+
     inputBox.value = '';
+
 }
