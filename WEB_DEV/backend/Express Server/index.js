86f96379094df18a7777afba6b3c04f173e06fe3
Joshua Biong <joshuabiong7@gmail.com>
2023-11-10 23:57:55 +0800

    adding deferent pages or routes

--- WEB_DEV/backend/Express Server/index.js
+++ WEB_DEV/backend/Express Server/index.js
@@ -5,6 +5,12 @@ const port = 3000
 app.get("/", (req, res) => {
     res.send("<h1>Hello World</h1>")
 })
+app.get("/about", (req, res) => {
+    res.send("<h1>Hello World</h1><p>Aspiring Web and mobile developer</p>")
+})
+app.get("/contact", (req, res) => {
+    res.send("<h1>Hello World</h1><p>Contact No. +63 997 208 8423</p>")
+})
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
